da366182c996778d908c4ba2a5989301af129cf5
Lea Lewis <77940920+le2310al@users.noreply.github.com>
2025-04-21 00:54:43 +0100

    clean up code

--- scraper/src/scraper/main.py
+++ scraper/src/scraper/main.py
@@ -10,10 +10,7 @@ from supabase import create_client, Client
 from supabase.client import ClientOptions
 import os
 
-
-# Accept cookies; possibly aids remaining undetected
 def init_db():
-    # Load environment variables from .env
     load_dotenv()
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
@@ -26,46 +23,46 @@ def init_db():
             schema="public",
         ))
 
-
 async def main():
+    # Initiate Supabase client
     supabase: Client = init_db()
-    # Initiate nodriver
+
+    # Initiate Nodriver scraper
     browser = await nd.start(headless = False, user_data_dir = "./profile")
     tab = await browser.get('https://jellycat.com/shop-all#/sort:ss_days_since_created:asc')
     await tab.sleep(3)
 
+    # Save cookies
     cookies = await (tab.query_selector("#onetrust-accept-btn-handler"))
     if cookies:
         await cookies.click()
-    popup = await tab.query_selector(".go3588653706")
-    if popup:
-        await popup.click()
     await browser.cookies.save()
     await tab.sleep(3)
     await tab.get_content()
-    current_page = 1
 
-    # Calculate number of pages based on total number of items; assumes 3x12 grid
+    # Calculate number of pages based on total number of items
     total_products_str = await tab.query_selector(".tw-mr-8")
     total_products_float = float(total_products_str.text) if total_products_str else 0
     total_pages = round(total_products_float / 36)
+    current_page = 1
+
     # Loop through all pages
     for i in range(total_pages):
         jellies = await tab.select_all(".product")
-        #print("PAGE " + str(current_page) + "ITEMS " + str(len(jellies)))
         for jelly in jellies:
             # Commit new jelly to database or update availability
             try:
-
                 card = await jelly.query_selector(".card-figure__link")
-                #Strips price from jellies name
+                # Strips price from jellies name
                 name = card.attrs["aria-label"].replace("'", "").split(",", 1)[0]
                 url = card.attrs["href"]
-                badge = await jelly.query_selector(".ss__badge")
-                availability = badge.text if badge else "Available"
+                # Stock status
                 button_disabled = await jelly.query_selector(".disabled")
                 stock = False if button_disabled else True
-                print("NAME: " + name + " URL: " + url + " STOCK: " + str(stock) + " AVAILABILITY: " + availability)
+                # Availability details
+                badge = await jelly.query_selector(".ss__badge")
+                availability = badge.text if badge else "Available"
+                # print("NAME: " + name + " URL: " + url + " STOCK: " + str(stock) + " AVAILABILITY: " + availability)
                 supabase.table("jellies").upsert(
                     {"name": name, "url": url, "stock": stock, "availability": availability},
                     on_conflict="name",
