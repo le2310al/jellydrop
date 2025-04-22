df1c4256a1867aace80f66a12dc980117434473a
lealewis <lealewis@Leas-Mac-mini.local>
2025-04-22 22:26:27 +0100

    fix eslint

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,37 +1,6 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import json from "@eslint/json";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import antfu from '@antfu/eslint-config'
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,vue}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,vue}"],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  {
-    files: ["**/*.vue"],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
-  },
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
-]);
+export default antfu({
+  formatters: true,
+  vue: true,
+})
